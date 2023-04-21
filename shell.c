22124933cb32d0867a8cdec6172b460037395d50
PatWork <tshupane@outlook.com>
2023-04-21 09:07:32 -0700

    ✨Simple shell Project

--- shell.c
+++ shell.c
@@ -1,52 +1,64 @@
 #include "shell.h"
 
-int main() {
-    char input[MAX_LENGTH];
-    char *args[MAX_LENGTH];
-    char *token;
-    int num_args;
-    pid_t pid;
-    int status, len;
+/**
+ * main - create a simple shell program.
+ * Author: Tshupane Morak & Ntuthuko Zimu
+ *
+ * return: 0.
+ */
+int main(void)
+{
+	char input[MAX_LENGTH];
+	char *args[MAX_LENGTH];
+	char *token;
+	int num_args, status, len;
+	pid_t pid;
 
-    while (1) {
-        printf("cisfun$ ");/*Display prompt*/
+	while (1)
+	{
+		printf("cisfun$ ");/*Display prompt*/
 
-        if (!fgets(input, MAX_LENGTH, stdin)) {
-            printf("\n");
-            break;/*If user presses Ctrl+D, exit the loop.*/
-        }
+		/*If user presses Ctrl+D, exit the loop.*/
+		if (!fgets(input, MAX_LENGTH, stdin))
+		{
+			printf("\n");
+			break;/*If user presses Ctrl+D, exit the loop.*/
+		}
 
-        len = strlen(input);
-        if (input[len - 1] == '\n') {
-            input[len - 1] = '\0';/*Remove trailing newline*/
-        }
+		/*Remove trailing newline*/
+		len = strlen(input);
+		if (input[len - 1] == '\n')
+			input[len - 1] = '\0';
 
-        if (strcmp(input, "exit") == 0) {
-            break;/*If user enters the "exit" command, break the loop.*/
-        }
+		/*If user enters the "exit" command, break the loop.*/
+		if (strcmp(input, "exit") == 0)
+			break;
 
-        num_args = 0;
-        token = strtok(input, " ");/*tokenizes the input string and save it*/
-        while (token != NULL) {
-            args[num_args++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[num_args] = NULL;
+		num_args = 0;
+		/*tokenizes the input string and save it*/
+		token = strtok(input, " ");
+		while (token != NULL)
+		{
+			args[num_args++] = token;
+			token = strtok(NULL, " ");
+		} args[num_args] = NULL;
 
-	/*Execute the command*/
-        pid = fork();
-        if (pid == 0) {/*Child process*/
-            execve(args[0], args, NULL);
-            printf("Error: Command not found\n");
-            exit(1);
-        } else if (pid < 0) {
-            printf("Fork failed\n");
-            exit(1);
-        } else {/* Parent process*/
-            waitpid(pid, &status, 0);
-        }
-    }
-
-    return 0;
+		/*Execute the command*/
+		pid = fork();
+		if (pid == 0)
+		{/*Child process*/
+			execve(args[0], args, NULL);
+			printf("Error: Command not found\n");
+			exit(1);
+		}
+		else if (pid < 0)
+		{
+			printf("Fork failed\n");
+			exit(1);
+		}
+		else
+		{/* Parent process*/
+			waitpid(pid, &status, 0);
+		}
+	} return 0;
 }
-
