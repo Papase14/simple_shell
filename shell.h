8f9b6ec4f9b281d384513ab447e43c48d58ed9e2
PatWork <tshupane@outlook.com>
2023-04-21 08:27:08 -0700

    ✨Prototypes

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+/* Headers required for the program */
+#include <stdio.h>
+#include <sys/types.h>/*getppid*/
+#include <unistd.h>/*getppid*/
+#include <sys/wait.h>/*wait system call*/
+#include <stdlib.h>
+#include <string.h>/*string*/
+#include <stddef.h>
+#include <stdio.h>
+
+#define MAX_LENGTH 1024
+
+#endif
+
