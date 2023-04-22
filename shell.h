d15fc6b055cabb084833ac9b611532f2083aee5e
PatWork <tshupane@outlook.com>
2023-04-22 06:22:11 -0700

    ✨Header with comments

--- shell.h
+++ shell.h
@@ -1,16 +1,18 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
-/* Headers required for the program */
-#include <stdio.h>
-#include <sys/types.h>/*getppid*/
-#include <unistd.h>/*getppid*/
-#include <sys/wait.h>/*wait system call*/
-#include <stdlib.h>
-#include <string.h>/*string*/
-#include <stddef.h>
-#include <stdio.h>
 
-#define MAX_LENGTH 1024
+/* Headers required for the program */
+#include <stdio.h>      /*Standard Input/Output library*/
+#include <string.h>     /*String manipulation functions*/
+#include <stdlib.h>     /*General purpose functions, including memory allocation*/
+#include <unistd.h>     /*Unix standard functions*/
+#include <sys/types.h>  /*Data types used in system calls*/
+#include <sys/wait.h>   /*Wait for process termination function*/
+
+#define MAX_LENGTH 1024 /*Maximum length of input string*/
+
+/*Prototypes*/
+
 
 #endif
 
