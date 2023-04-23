cdcd566911aa18d5a398e2cc0bc2b16b4879ab97
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-04-23 18:52:59 +0200

    🚨Code reverted to original. strtok() need fixing

--- shell.c
+++ shell.c
@@ -43,11 +43,11 @@ int main(void)
 		}
 
 		num_args = 0; /*Initialize number of arguments to 0*/
-		token = _strtok(input, " "); /*Get first token from input string*/
+		token = strtok(input, " "); /*Get first token from input string*/
 		while (token != NULL)
 		{
 			args[num_args++] = token;
-			token = _strtok(NULL, " ");
+			token = strtok(NULL, " ");
 		} args[num_args] = NULL;
 
 		pid = fork(); /*Create child process*/
