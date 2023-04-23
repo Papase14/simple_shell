37339bdbae62070eabe1c89017fdce2673cac2e1
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-04-23 19:48:18 +0200

    🛠️Testing

--- strtok.c
+++ strtok.c
@@ -7,32 +7,52 @@
  *
  * Return: token
  */
-char *_strtok(char *string, const char *delimiter)
+char *_strtok(char **string, const char *delimiter)
 {
-    static char *last_String = NULL;
-    char *_token = NULL;
+    static char *lastToken = NULL;  /* State variable */
+    char *delimiterFound = NULL;
+    int tokenLength = 0;
+    char *tok = NULL;
+    char *start = NULL;
 
-    if (string == NULL && last_String == NULL) {
+    if (string == NULL || *string == NULL) {
+        start = lastToken;
+    } else {
+        start = *string;
+    }
+
+    /*Find the next delimiter*/
+    delimiterFound = strstr(start, delimiter);
+
+    /*Calculate the length of the token*/
+    if (delimiterFound) {
+        tokenLength = delimiterFound - start;
+    } else {
+        tokenLength = strlen(start);
+    }
+
+    /*Allocate memory for the token*/
+    tok = malloc(tokenLength + 1);
+    if (tok == NULL) {
         return NULL;
     }
 
-    if (string == NULL) {
-        string = last_String;
+    /*Copy the token*/
+    memcpy(tok, start, tokenLength);
+    tok[tokenLength] = '\0';
+
+    /*Update the state*/
+    if (delimiterFound) {
+        lastToken = delimiterFound + strlen(delimiter);
+    } else {
+        lastToken = start + tokenLength;
     }
 
-    _token = string + strspn(string, delimiter);
-
-    /*if reachs the end of the string, return NULL*/
-    if (*_token == '\0') {
-        last_String = NULL;
-        return NULL;
+    /*Update the output parameter*/
+    if (string != NULL) {
+        *string = lastToken;
     }
 
-    last_String = _token + strcspn(_token, delimiter);
-
-    if (*last_String != '\0') {
-        *last_String++ = '\0';
-    }
-
-    return _token;
+    /*Return the token*/
+    return tok;
 }
