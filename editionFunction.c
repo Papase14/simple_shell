a4d8dae6d36b86a8106d342204305b4e195d0967
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-04-25 18:05:43 +0200

    🛠️Fixed _puts function. Newline remove

--- editionFunction.c
+++ editionFunction.c
@@ -20,16 +20,14 @@ int _putchar(char c)
  */
 void _puts(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (*str != '\0')
 	{
 		_putchar(*str);
 		i++;
 		str++;
 	}
-	_putchar('\n');
 }
 
 /**
