c60d6aeee2f0ef5d6be0204abcbc8c74c60f008c
Ntuthuko <ntuthukolu@gmail.com>
2023-04-25 13:50:44 -0700

    Please work

---
+++ strtok.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * _strtok - splits a string based on delimiter given
+ * @string: string it takes in
+ * @delimiter: specifies set of bytes that delimit the parsed string
+ * Authour: Tshupane Morake & Ntuthuko Zimu
+ *
+ * Return:( token
+ */
+char *_strtok(char *string, const char *delimiter)
+{
+	static char *last_String;
+	char *_token = 0;
+
+	if (string == NULL && last_String == NULL)
+	{
+		return (NULL);
+	}
+
+	if (string == NULL)
+	{
+		string = last_String;
+	}
+
+	_token = string + strspn(string, delimiter);
+	/* If reaches the end of the string, return NULL */
+	if (*_token == '\0')
+	{
+		last_String = NULL;
+		return (NULL);
+	}
+
+	last_String = _token + strcspn(_token, delimiter);
+	if (*last_String != '\0')
+	{
+		*last_String++ = '\0';
+	}
+	return (_token);
+}
